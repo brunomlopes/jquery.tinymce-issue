e61de473a7b6514cdba57825e1e98b5f39f630dd
Bruno Lopes <brunomlopes@gmail.com>
2017-07-28 15:28:36 +0100

    Well npm/jspm install doesn't seem to want to work

--- kudu.cmd
+++ kudu.cmd
@@ -1,6 +1,6 @@
 :: ensure packages are installed
-npm install jspm -g
-npm install jspm --save-dev
-jspm install
+:: npm install jspm -g
+:: npm install jspm --save-dev
+:: jspm install
 :: copy all sources to deployment
 xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /Y
